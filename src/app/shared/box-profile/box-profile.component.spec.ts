11bbacd4a90e7914f43ca4f3b7e18eb14c1a21de
Arthur Santana <arthurgiannonisantana@yahoo.com.br>
2023-08-24 19:30:48 -0300

    feat: adicionando todos os icones do projeto

---
+++ src/app/shared/box-profile/box-profile.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { BoxProfileComponent } from "./box-profile.component";
+
+describe("BoxProfileComponent", () => {
+  let component: BoxProfileComponent;
+  let fixture: ComponentFixture<BoxProfileComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [BoxProfileComponent]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(BoxProfileComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shared/box-profile/box-profile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+@Component({
+  selector: "app-box-profile",
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: "./box-profile.component.html",
+  styleUrls: ["./box-profile.component.scss"]
+})
+export class BoxProfileComponent {}
